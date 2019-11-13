2dbcd8f273bf7fc6519afd7c3cc1fdee75757d87
ausaf-a <ausaf100@gmail.com>
2019-11-12 21:15:25 -0500

    incorporated testing, need to add logging for results

--- bench.py
+++ bench.py
@@ -6,7 +6,7 @@ import time
 timeSent = -1
 
 class Callbacks(CallbackSet):
-
+	
     def on_state_changed(self, previous_state, current_state):
         if current_state == chirpsdk.CHIRP_SDK_STATE_STOPPED:
             print("ChirpSDK has stopped")
@@ -26,28 +26,31 @@ class Callbacks(CallbackSet):
         if payload is None:
             print('Decode failed')
         else:
-            print('Received data')
+            print('Received data: ', payload)
 
 def init(blockname='default'):
     global timeSent 
     
     chirp = ChirpSDK(block=blockname)
-    chirp.start(send=True)
-
+    chirp.start(send=True, receive=False)
+    print('send initialized')
     chirp2 = ChirpSDK(block=blockname)
     chirp2.input_sample_rate = 48000 #USB mic requires 48Khz to function
+    chirp2.set_callbacks(Callbacks())    
     chirp2.start(send=False, receive=True)
-    chirp2.set_callbacks(Callbacks())
+    print('recv initialized')
+    while True:
+        sendTest(chirp, blockname)
+        time.sleep(2)
 
-    msg = 'hello darkness'
+def sendTest(sdk, blockname):
+    global timeSent
+    msg = 'a'
     payload = bytearray([ord(ch) for ch in msg])
     print("Sending",len(msg),"bytes using",blockname,"configuration.")    
-    
     timeSent = time.time()
-    chirp.send(payload, blocking=False)
+    sdk.send(payload, blocking=True)
     
-    while True: 
-        time.sleep(0.1)
-    chirp.stop()
-    chirp2.stop()
+
+
 init()
